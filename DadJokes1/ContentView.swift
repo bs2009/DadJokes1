e343072674363da6bfb1a4b9d0992755a57e0d74
Bill Song <bsong2009@gmail.com>
2019-09-26 06:27:44 -0500

    Initial Commit

---
+++ DadJokes1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DadJokes1
+//
+//  Created by William Song on 9/26/19.
+//  Copyright © 2019 awa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
