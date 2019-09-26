f8b139543f2054683c45b4f7655dd864807d451f
Bill Song <bsong2009@gmail.com>
2019-09-26 06:58:06 -0500

    first commit, display raw jokes

--- DadJokes1/ContentView.swift
+++ DadJokes1/ContentView.swift
@@ -8,9 +8,25 @@
 
 import SwiftUI
 
+struct Joke {
+    var setup: String
+    var punchline: String
+    var rating: String
+}
 struct ContentView: View {
+    
+    let jokes = [
+        Joke(setup: "What's a cow's favourite place?", punchline: "A mooseum.", rating: "Silence"),
+        Joke(setup: "What's brown and stick?", punchline: "A stick.", rating: "Sign"),
+        Joke(setup: "What's orange and sounds like a parrot?", punchline: "A carrot.", rating: "Smirk")
+    
+]
     var body: some View {
-        Text("Hello World")
+        List {
+            ForEach(jokes, id: \.setup) { joke in
+                Text(joke.setup)
+            }
+        }
     }
 }
 
